c49970f9d6c1d0e3080eb93a0b8107e9cd52a0d6
mrsdno <christine@dno.dev>
2022-07-24 20:17:56 -0400

    css styling and some content updates

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,10 +1,19 @@
-var toggler = document.getElementsByClassName("caret");
-var i;
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
 
-for (i = 0; i < toggler.length; i++) {
-    toggler[i].addEventListener("click", function() {
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
 
-this.parentElement.querySelector(".nested").classList.toggle(active);
-        this.classList.toggle("carret-down");
-    });
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
 }
+
+window.addEventListener("scroll", reveal);
+
+reveal();
